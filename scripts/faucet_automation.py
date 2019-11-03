4544c187df4a82a9c1232c69d740689ff3269916
Vaclav Pospisil <pospisil.vaclav@gmail.com>
2019-11-03 19:29:28 +0100

    finished

--- scripts/faucet_automation.py
+++ scripts/faucet_automation.py
@@ -11,55 +11,52 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import re
 
 # run headless
 # options = Options()
 # options.add_argument('--headless')
-# options.add_argument('--disable-gpu')  # Last I checked this was necessary.
-driver = webdriver.Chrome('./chromedriver/chromedriverForWin.exe')
+# options.add_argument('--disable-gpu')
+driver = webdriver.Chrome('./chromedrivers/chromedriverForWin.exe')
 
 url = 'http://bonusbitcoin.co/faucet'
 driver.get(url)
 
 # driver.maximize_window()
 
-searchPickupPlace = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#PageContent_SignInButton')))
-searchPickupPlace.click()
+try:
+    # if a user is signed in, sign in process is skipped
+    signIn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#PageContent_SignInButton')))
+    signIn.click()
+
+    time.sleep(1)
+    setEmail = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#SignInEmailInput')))
+    setEmail.clear()
+    setEmail.send_keys('pospisil1vaclav@gmail.com')
+
+    time.sleep(1)
+    setPassw = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#SignInPasswordInput')))
+    setPassw.send_keys('childbycrystalcastle1+')
+
+    time.sleep(1)
+    reCaptcha = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.recaptcha-checkbox-border')))
+    reCaptcha.click()
+
+    time.sleep(1)
+    signInConfirm = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-bind="click: signIn"]')))
+    signInConfirm.click()
+    
+except Exception as e:
+    print(e)
+    pass
 
 time.sleep(1)
-setPickupPlace = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#search-results__seznam .pickup_box')))
-setPickupPlace.click()
+reCaptchaClaimer = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.recaptcha-checkbox-border')))
+reCaptchaClaimer.click()
 
-time.sleep(1)
-setPickupPlace2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[title="Kulhavého 669/2, Praha 4"]')))
-setPickupPlace2.click()
+claimer = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '[data-bind="click: delayedClaim, text: claimButtonText, enable: canClaimNow"]')))
+ActionChains(driver).move_to_element(claimer).click()
 
-time.sleep(1)
-confirmPickupPlace = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#pickupPlace-0-739 .selected-pickup-place-confirm')))
-confirmPickupPlace.click()
-
-time.sleep(1)
-setPaymentMethod = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#paymentContainer-101')))
-setPaymentMethod.click()
-
-time.sleep(1)
-confirmOrder = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#confirmOrder2Button')))
-confirmOrder.click()
-
-# set email and phone
-time.sleep(1)
-setEmail = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#userEmail')))
-setEmail.clear() # clear predefined value ('@')
-setEmail.send_keys('hamcouz@gmail.com')
-
-setPhoneNumber = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#userPhone')))
-setPhoneNumber.send_keys('666555444')
-
-# finalize order
-finalize = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.js-order3-continue')))
-finalize.click()
 
 # close browser
-driver.quit()
+# driver.quit()
 
